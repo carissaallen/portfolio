f7edb0fd921dd23126f030ffdde0c63c6767ce9e
Carissa Allen <carissawood@outlook.com>
2019-06-06 17:37:23 -0700

    Add progress for blog with mongoDB

--- server.js
+++ server.js
@@ -1,31 +1,67 @@
 // server.js - portfolio route module
 
 const express = require('express');
-const port = process.env.PORT;
+const port = 3000 // process.env.PORT;
 const app = express();
-const router = express.Router();
 const path = __dirname + '/views/';
 
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/post", { useNewUrlParser: true });
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    created: Date
+ });
+var Post = mongoose.model('Post', postSchema);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname)); // serves static files
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-router.get('/', function(req, res) {
-    res.sendFile(path + 'index.html');
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-router.get('/about', function(req, res) {
-    res.sendFile(path + 'about.html');
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
-router.get('/portfolio', function(req, res) {
-    res.sendFile(path + 'portfolio.html');
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio');
 });
 
-router.get('/contact', function(req, res) {
-    res.sendFile(path + 'contact.html');
+app.get('/blog', (req, res) => {
+    Post.find({}, (err, posts) => {
+        res.render('blog', { posts: posts })
+    });
 });
 
-app.use('/', router);
-app.listen(port);
+app.get('/post', (req, res) => {
+        res.render('post');
+});
+
+app.post('/add_post', (req, res) => {
+    var postData = new Post(req.body);
+    postData.save().then( result => {
+        res.redirect('/blog');
+    }).catch(err => {
+        res.status(400).send("Unable to save data");
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port...`);
+});
 
 // future work: 
 // should add error page to handle erroneous requests--
